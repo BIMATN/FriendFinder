c1182151235688d7e2be07051ca4a2cd4907adb2
Benjamin Rodriguez <bimatn@outlook.com>
2017-10-17 15:28:08 -0700

    routes set up and server activated

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,14 +1,21 @@
 //routes for api
 const express = require('express');
-const router = express.router();
+const router = express.Router();
+const path = require("path");
+const app = express();
 
 router.get('/api/friends', function(req, res){
 	//displays json of all friends
+	console.log("ALERT: Friends api data retrieved by user");
+	res.json()
 });
 
 router.post('/api/friends', function(req, res){
 	//incoming survey results
 	//compatibility logic
+	console.log("ALERT: Friends api data posted by user");
+	console.log(req.body);
+	//console.log(res);
 });
 
 module.exports = router;
--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -1,13 +1,16 @@
 //routes for html page
 const express = require('express');
-const router = express.router();
+const router = express.Router();
+var path = require("path");
 
 router.get('/survey', function(req, res){
-
+	console.log("ALERT: Survey Page has been accessed by a user");
+	res.sendFile(path.join(__dirname, "..", "public/survey.html"));
 });
 
 router.get('/', function(req, res){
-
+	console.log("ALERT: Home page has been accessed by a user");
+	res.sendFile(path.join(__dirname, "..", "public/home.html"));
 });
 
 module.exports = router;
